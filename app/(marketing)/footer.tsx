c6a11ed81f14c68ff63b3dca8f7e66ab6205e9cd
Cristian <cristianprodius1@gmail.con>
2024-11-27 22:00:17 +0200

    scheleton for lean page

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,9 +1,42 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-t">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/math.png"
+            alt="mathematics"
+            width={40}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Matematica
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/history.png"
+            alt="mathematics"
+            width={40}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Istorie
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/geo.png"
+            alt="mathematics"
+            width={40}
+            height={40}
+            className="mr-4 rounded-md"
+          />
+          Geometrie
+        </Button>
+      </div>
     </footer>
   );
 };
