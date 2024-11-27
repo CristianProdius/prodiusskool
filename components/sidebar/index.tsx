18ce654ef9d5cae231a1f3a078151c4770af1841
Cristian <cristianprodius1@gmail.con>
2024-11-27 22:28:58 +0200

    sidebar finished

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,4 +1,9 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SideBarItem } from "./sideBarItem";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -7,11 +12,36 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex  bg-blue-300 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex   h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.png" alt="logo" width={40} height={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            ProdiusSkool
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SideBarItem label="Invata" href="/learn" iconSrc="/learn.png" />
+        <SideBarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.png"
+        />
+        <SideBarItem label="teste" href="/quest" iconSrc="/quest.png" />
+        <SideBarItem label="magazin" href="/store" iconSrc="/store.png" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
