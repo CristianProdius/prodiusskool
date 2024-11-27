e589186303d8ccac7071ff1765ec26e72cd614b1
Cristian <cristianprodius1@gmail.con>
2024-11-27 20:32:31 +0200

    autentidfication

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,9 +1,44 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignIn,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { SignedOut, SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-2-- px-4">
-      Header
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.png" alt="logo" width={40} height={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            ProdiusSkool
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button size="lg" variant="ghost">
+                Sign In
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,53 @@
 import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>HomePge</h1>
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.png" fill alt="hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Invata si practica ca so poti rupe bacul cu ProdiusSkool
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Incepe acum
+                </Button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedOut>
+              <SignInButton mode="modal">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  Am deja cont, is crutoi acum
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/lern">Continua sa fii crutoi</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
